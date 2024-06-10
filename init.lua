3b5cf0ec71be232cc7d0e51cb712ce9d8ba0a831
Balazs Horvath <acsipont@gmail.com>
2024-06-10 13:48:46 +0200

    awoo

--- init.lua
+++ init.lua
@@ -45,31 +45,14 @@ end)
 require("neo-tree")
 
 require'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = {"c", "lua", "vim", "vimdoc", "query"},
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
-
-    -- List of parsers to ignore installing (or "all")
-    ignore_install = {"javascript"},
-
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
     highlight = {
         enable = true,
 
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        disable = {"c", "rust"},
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- disable = {"c", "rust"},
         disable = function(lang, buf)
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -83,5 +66,43 @@ require'nvim-treesitter.configs'.setup {
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false
+    }
+}
+
+require'nvim-web-devicons'.setup {
+    color_icons = true,
+    default = true,
+    -- globally enable "strict" selection of icons - icon will be looked up in
+    -- different tables, first by filename, and if not found by extension; this
+    -- prevents cases when file doesn't have any extension but still gets some icon
+    -- because its name happened to match some extension (default to false)
+    strict = true,
+    -- same as `override` but specifically for overrides by filename
+    -- takes effect when `strict` is true
+    override_by_filename = {
+        [".gitignore"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "Gitignore"
+        }
+    },
+    -- same as `override` but specifically for overrides by extension
+    -- takes effect when `strict` is true
+    override_by_extension = {
+        ["log"] = {
+            icon = "",
+            color = "#81e043",
+            name = "Log"
+        }
+    },
+    -- same as `override` but specifically for operating system
+    -- takes effect when `strict` is true
+    override_by_operating_system = {
+        ["apple"] = {
+            icon = "",
+            color = "#A2AAAD",
+            cterm_color = "248",
+            name = "Apple"
+        }
     }
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,7 @@
-return {{
+return {{{"MunifTanjim/nui.nvim"}, "nvim-lua/plenary.nvim"}, {"nvim-tree/nvim-web-devicons"}, {"ibhagwan/fzf-lua"},
+        {"mfussenegger/nvim-dap"}, {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
+    branch = "v3.x"
 }, {
     "stevearc/conform.nvim",
     event = 'BufWritePre', -- format on save
