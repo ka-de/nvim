f57f0fd7ef6476fcb765e577502d61bc9a1aef5a
Balazs Horvath <acsipont@gmail.com>
2024-06-10 14:51:37 +0200

    stylua

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-    local repo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system {"git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath}
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -14,21 +14,25 @@ vim.opt.rtp:prepend(lazypath)
 local lazy_config = require "configs.lazy"
 
 -- load plugins
-require("lazy").setup({{
+require("lazy").setup({
+  {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000
-}, {
+    priority = 1000,
+  },
+  {
     "NvChad/NvChad",
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
     config = function()
-        require "options"
-    end
-}, {
-    import = "plugins"
-}}, lazy_config)
+      require "options"
+    end,
+  },
+  {
+    import = "plugins",
+  },
+}, lazy_config)
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
@@ -39,70 +43,70 @@ vim.cmd.colorscheme "catppuccin-macchiato"
 require "nvchad.autocmds"
 
 vim.schedule(function()
-    require "mappings"
+  require "mappings"
 end)
 
-require("neo-tree")
+require "neo-tree"
 
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "vim", "vimdoc", "query"},
-    sync_install = false,
-    auto_install = true,
+require("nvim-treesitter.configs").setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+  sync_install = false,
+  auto_install = true,
 
-    highlight = {
-        enable = true,
+  highlight = {
+    enable = true,
 
-        -- disable = {"c", "rust"},
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
+    -- disable = {"c", "rust"},
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false
-    }
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
 }
 
-require'nvim-web-devicons'.setup {
-    color_icons = true,
-    default = true,
-    -- globally enable "strict" selection of icons - icon will be looked up in
-    -- different tables, first by filename, and if not found by extension; this
-    -- prevents cases when file doesn't have any extension but still gets some icon
-    -- because its name happened to match some extension (default to false)
-    strict = true,
-    -- same as `override` but specifically for overrides by filename
-    -- takes effect when `strict` is true
-    override_by_filename = {
-        [".gitignore"] = {
-            icon = "",
-            color = "#f1502f",
-            name = "Gitignore"
-        }
+require("nvim-web-devicons").setup {
+  color_icons = true,
+  default = true,
+  -- globally enable "strict" selection of icons - icon will be looked up in
+  -- different tables, first by filename, and if not found by extension; this
+  -- prevents cases when file doesn't have any extension but still gets some icon
+  -- because its name happened to match some extension (default to false)
+  strict = true,
+  -- same as `override` but specifically for overrides by filename
+  -- takes effect when `strict` is true
+  override_by_filename = {
+    [".gitignore"] = {
+      icon = "",
+      color = "#f1502f",
+      name = "Gitignore",
     },
-    -- same as `override` but specifically for overrides by extension
-    -- takes effect when `strict` is true
-    override_by_extension = {
-        ["log"] = {
-            icon = "",
-            color = "#81e043",
-            name = "Log"
-        }
+  },
+  -- same as `override` but specifically for overrides by extension
+  -- takes effect when `strict` is true
+  override_by_extension = {
+    ["log"] = {
+      icon = "",
+      color = "#81e043",
+      name = "Log",
     },
-    -- same as `override` but specifically for operating system
-    -- takes effect when `strict` is true
-    override_by_operating_system = {
-        ["apple"] = {
-            icon = "",
-            color = "#A2AAAD",
-            cterm_color = "248",
-            name = "Apple"
-        }
-    }
+  },
+  -- same as `override` but specifically for operating system
+  -- takes effect when `strict` is true
+  override_by_operating_system = {
+    ["apple"] = {
+      icon = "",
+      color = "#A2AAAD",
+      cterm_color = "248",
+      name = "Apple",
+    },
+  },
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,22 +1,30 @@
-return {{{"MunifTanjim/nui.nvim"}, "nvim-lua/plenary.nvim"}, {"nvim-tree/nvim-web-devicons"}, {"ibhagwan/fzf-lua"},
-        {"mfussenegger/nvim-dap"}, {
+return {
+  { { "MunifTanjim/nui.nvim" }, "nvim-lua/plenary.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
+  { "ibhagwan/fzf-lua" },
+  { "mfussenegger/nvim-dap" },
+  {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x"
-}, {
+    branch = "v3.x",
+  },
+  {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- format on save
+    event = "BufWritePre", -- format on save
     config = function()
-        require "configs.conform"
-    end
-}, {
+      require "configs.conform"
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
-        require("nvchad.configs.lspconfig").defaults()
-        require "configs.lspconfig"
-    end
-}, {
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-        ensure_installed = {"vim", "lua", "vimdoc", "html", "css"}
-    }
-}}
+      ensure_installed = { "vim", "lua", "vimdoc", "html", "css" },
+    },
+  },
+}
